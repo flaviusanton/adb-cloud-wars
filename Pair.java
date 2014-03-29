87f9d151b45cb90490345af20c55b821bbb132f3
Flavius Anton <f.v.anton@gmail.com>
2014-03-29 20:38:27 +0200

    Add pair class for ints.

---
+++ Pair.java
@@ -0,0 +1,12 @@
+/**
+ * Created by fanton on 29/03/14.
+ */
+public class Pair {
+    public Integer fst;
+    public Integer snd;
+
+    public Pair(Integer fst, Integer snd) {
+        this.fst = fst;
+        this.snd = snd;
+    }
+}
